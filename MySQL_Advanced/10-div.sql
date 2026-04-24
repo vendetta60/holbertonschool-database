41d775ba34ce0d123a5b036f4d8f2a56d08f5aec
vendetta60 <115636143+vendetta60@users.noreply.github.com>
2026-04-24 15:42:18 +0400

    Update 10-div.sql

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,17 +1,18 @@
 -- Task 10: Safe divide - creates a function SafeDiv that divides
 -- (and returns) the first by the second number
 -- or returns 0 if the second number is equal to 0
-DELIMITER |
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (a INT, b INT)
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS SafeDiv$$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    DECLARE result FLOAT;
     IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
+        RETURN 0;
     END IF;
-    RETURN result;
-END;
-|
+    RETURN a / b;
+END$$
+
+DELIMITER ;
